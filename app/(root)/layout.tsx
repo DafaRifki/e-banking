f20fb1f56a0fe80e4d0996e18c38b5bca844075a
Dafs <rfkdafa03@gmail.com>
2025-01-01 16:31:08 +0700

    nextjs v14

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import Sidebar from "@/components/Sidebar";
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+    const loggedIn = { firstName: "Hydra", lastName: "LX" };
+
+  return (
+   <main className="flex h-screen w-full font-inter">
+    <Sidebar user={loggedIn}/>
+    {children}
+   </main>
+  );
+}
